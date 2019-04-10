163a5702c4acf4402150522a4553c44df30927ec
Nicolas Zermati <nicoolas25@gmail.com>
2019-04-10 20:12:39 +0200

    Start recursivity implementation

--- change.rb
+++ change.rb
@@ -4,10 +4,12 @@ class Change
 
   def self.rec_coin(total_amount, coin_types)
     return 0 if total_amount.zero?
-
+    return 1 if coin_types.any? { |t| t == total_amount }
     raise StandardError unless coin_types.min <= total_amount
 
-    4
+    eligible_coins = coin_types.select { |t| t <= total_amount }
+    biggest_coin = eligible_coins.max
+    1 + rec_coin(total_amount - biggest_coin, coin_types)
   end
 
 end
@@ -25,4 +27,13 @@ class ChangeTest < MiniTest::Test
   def test_compatible_amount_and_coin
     assert_raises { Change.rec_coin(5, [10, 20]) }
   end
+
+  def test_step_1
+    assert_equal(1, Change.rec_coin(1, [1]))
+  end
+
+  def test_recursivity
+    assert_equal(2, Change.rec_coin(3, [1, 2]))
+  end
+
 end
